2e8170cfca736b505ac6bf49637269fa6df19f7e
Chales Horn <charles.horn@gmail.com>
2023-04-30 21:26:01 +1200

    more refactoring

--- demos/menger.sh
+++ demos/menger.sh
@@ -8,59 +8,60 @@ function hook_ping() {
 	send_packet "00" "$res"
 }
 
-function sub() {
+function swp() {
 	sed "s/U/V/g;s/T/U/g" <<< $1
+}
+
+function blk() {
+	sed 's/X/13/g;s/S/00/g' <<< $1
 }
 
 function hook_chunks() {
 	# Build 27x27 sponge:
-	sponge=$(sed 's/A/CDC/g;s/B/EFE/g;
+	n="N9V9V9VN /g;s/N/"
+	o=" TVT3V TVTUVU3V UVUTVT3V TVT "
+	a="N3TVT3UVU3TVTN /g;s/N/9T9U9T /g"
+	b="N${o}N /g;s/N/3T3V3T3U3V3U3T3V3T /g"
+	e="$n 3TVT3UVU3TVT /g"
+	f="$n$o/g"
+	c=$(swp "$a")
+	d=$(swp "$b")
+	g=$(swp "$e")
+	h=$(swp "$f")
+	sponge=$(sed "s/A/CDC/g;s/B/EFE/g;
 		s/\([CDEF]\)/\11\12\11/g;
 		s/C1/aba/g;s/C2/cdc/g;
 		s/D1/efe/g;s/D2/ghg/g;
 		s/E1/bmb/g;s/E2/dmd/g;
-		s/F1/fmf/g;s/F2/hmh/g' <<< ABA)
-	n="N9V9V9VN /g;s/N/"
-	o=" TVT 3V TVT UVU 3V UVU TVT 3V TVT "
-	a="N3TVT3UVU3TVTN /g;s/N/9T9U9T /g"
-	b="N${o}N /g;s/N/3T3V3T3U3V3U3T3V3T /g"
-	e="$n TVTTVTTVT UVUUVUUVU TVTTVTTVT /g"
-	f="$n$o/g"
-	c=$(sub "$a")
-	d=$(sub "$b")
-	g=$(sub "$e")
-	h=$(sub "$f")
-	sponge=$(sed "$(for i in {a..h}; do printf "s/${i}/${!i};";done)" <<< $sponge)
-	sponge=$(sed "s/m/$(repeat 81 V)/g" <<< $sponge)
-	sponge=$(sed "s/9\([A-Z]*\)/3\13\13\1 /g;s/3\([A-Z]*\)/\1\1\1 /g" <<< $sponge)
-	sponge=$(sed 's/\s//g;s/T/XXX/g;s/U/XSX/g;s/V/SSS/g' <<< $sponge)
+		s/F1/fmf/g;s/F2/hmh/g;
+		$(for i in {a..h}; do printf "s/${i}/${!i};";done)
+		s/m/$(repeat 81 V)/g;
+		s/9\([A-Z]*\)/3\13\13\1 /g;s/3\([A-Z]*\)/\1\1\1 /g;
+		s/\s//g;s/T/XXX/g;s/U/XSX/g;s/V/SSS/g" <<< ABA)
+		#          Triple/   Un/       Void
 
 	# Split sponge into 4 16x16 chunks:
 	i=0
+	gap=$(repeat 48 00)
 	while read row; do
 		read a b < <(sed "s/^\(.\{6\}\)\(.\{8\}\)\(.\{8\}\)\(.\{5\}\)$/\2SS\1 \4SSS\3/" <<< $row)
 		log $row $i
 		if (( i == 0 )); then
-		  one+=$(printf '00%.0s' {1..32})
-		  two+=$(printf '00%.0s' {1..32})
+		  one+=$(repeat 32 00)
+		  two+=$(repeat 32 00)
 		fi
 		if (( i < 14 )); then
-		  one+=$(sed 's/X/13/g;s/S/00/g' <<< $a)
-		  two+=$(sed 's/X/12/g;s/S/00/g' <<< $b)
+		  one+=$(blk "$a")
+		  two+=$(blk "$b")
 		else
-		  tri+=$(sed 's/X/13/g;s/S/00/g' <<< $a)
-		  tet+=$(sed 's/X/13/g;s/S/00/g' <<< $b)
+		  tri+=$(blk "$a")
+		  tet+=$(blk "$b")
 		fi
 		((i+=1))
 		if (( i == 27 )); then
-		  i=0
-		  tri+=$(printf '00%.0s' {1..48})
-                  tet+=$(printf '00%.0s' {1..48})
+		  i=0; tri+=$gap; tet+=$gap
 	       	fi
 	done < <(fold -w27 <<< $sponge)
-
-	# and split chunk cols into 16x16x16 chunk sections 
-	# since idk how to render full cols...
 
 	#readarray -t one < <(fold -w8192 <<< $one)
 	chunk_header
@@ -97,19 +98,19 @@ function hook_chunks() {
 	chunk_footer
 	echo "$chunk" > $TEMP/world/FFFFFFFF00000001
 
-	pkt_chunk FFFFFFFF FFFFFFFF
+	#pkt_chunk FFFFFFFF FFFFFFFF 00
 	pkt_chunk FFFFFFFF 00000000  # two
 	pkt_chunk FFFFFFFF 00000001  # four
-	pkt_chunk FFFFFFFF 00000002 00
-	pkt_chunk FFFFFFFF 00000003 00
+	#pkt_chunk FFFFFFFF 00000002 00
+	#pkt_chunk FFFFFFFF 00000003 00
 
-	pkt_chunk 00000000 FFFFFFFF 00
+	#pkt_chunk 00000000 FFFFFFFF 00
 	pkt_chunk 00000000 00000000   # one
 	pkt_chunk 00000000 00000001   # three
-	pkt_chunk 00000000 00000002 00
-	pkt_chunk 00000000 00000003 00
+	#pkt_chunk 00000000 00000002 00
+	#pkt_chunk 00000000 00000003 00
 	
-	pkt_chunk 00000001 FFFFFFFF 00
-	pkt_chunk 00000001 00000000 00
-	pkt_chunk 00000001 00000001 00
+	#pkt_chunk 00000001 FFFFFFFF 00
+	#pkt_chunk 00000001 00000000 00
+	#pkt_chunk 00000001 00000001 00
 }
